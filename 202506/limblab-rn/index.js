759246d9b9da9294fa23868d5c9b83591bb30e54
kapiljxanthus <kapilj@xanthus.in>
2025-08-26 17:33:29 +0000

    Merged from main branch

--- 202506/limblab-rn/index.js
+++ 202506/limblab-rn/index.js
@@ -2,26 +2,26 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import {
-    Platform,
-    PermissionsAndroid
+  Platform,
+  PermissionsAndroid
 } from "react-native"
 
 import NotificationService from './src/utils/NotificationService';
 import PushNotificationIOS from "@react-native-community/push-notification-ios"
 import AsyncStorage from "@react-native-async-storage/async-storage"
 
-const onRegister = async (token) => {
+ const onRegister = async (token) => {
     await AsyncStorage.setItem('deviceToken', token.token)
-}
-
-const onNotif = (notification) => {
+  }
+/*
+  const onNotif = (notification) => {
+    console.log('Notification received:', notification);
     notification.finish(PushNotificationIOS.FetchResult.NoData);
-}
+  }*/
 
-new NotificationService(
-    onRegister,
-    onNotif,
-);
+  new NotificationService(
+      onRegister
+    );
 
 if (Platform.OS === "android") PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS)
 
